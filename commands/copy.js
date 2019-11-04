f4559abc82d1b7df42ea25c0d1a25e66ef6bbe1e
Jérémy Lal <kapouer@melix.org>
2019-11-04 10:24:28 +0100

    Use copy module for copy

--- commands/copy.js
+++ commands/copy.js
@@ -1,26 +1,8 @@
-const fs = require("fs").promises;
-const Path = require('path');
+const copy = require("@danieldietrich/copy");
 
 module.exports = function(inputs, output) {
 	return Promise.all(inputs.map(function(input) {
-		const dirs = {};
-		return copy(dirs, input, output);
+		return copy(input, output);
 	}));
 };
 
-function copy(dirs, from, to) {
-	console.error("copy", from, to);
-	return fs.stat(from).then(function(stat) {
-		if (stat.isDirectory()) return;
-		const dir = Path.dirname(to);
-		return Promise.resolve().then(() => {
-			if (dirs[dir]) return;
-			dirs[dir] = true;
-			return fs.mkdir(dir, {
-				recursive: true
-			});
-		}).then(function() {
-			return fs.copyFile(from, to);
-		});
-	});
-}
