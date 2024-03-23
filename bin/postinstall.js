3f04693f58b1dd54602ae2804f4701ab536457e6
Jérémy Lal <kapouer@melix.org>
2024-03-23 11:27:17 +0100

    Modernize

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,29 +1,34 @@
 #!/usr/bin/env node
 
-var postinstall = require('../');
+const postinstall = require('../');
 
-var fs = require('fs').promises;
-var minimist = require('minimist');
+const { promises: fs } = require('node:fs');
+const minimist = require('minimist');
 
-var opts = minimist(process.argv, {
+const opts = minimist(process.argv, {
 	alias: {
 		allow: 'a'
 	}
 });
-if (opts.allow && typeof opts.allow == "string") opts.allow = [opts.allow];
+
+if (opts.allow && typeof opts.allow == "string") {
+	opts.allow = [opts.allow];
+}
+
 delete opts.a;
 delete opts.cwd;
 
-var configFile = opts._.length == 3 && opts._[2] || "package.json";
+const configFile = opts._.length == 3 && opts._[2] || "package.json";
 delete opts._;
 
-
-return fs.readFile(configFile).then(JSON.parse).then(function(obj) {
-	return postinstall.process(obj[process.env.npm_lifecycle_event] || {}, opts);
-}).then(function() {
-	process.exit(0);
-}).catch(function(err) {
-	console.error(err);
-	process.exit(1);
-});
-
+(async () => {
+	const buf = await fs.readFile(configFile);
+	const obj = JSON.parse(buf);
+	try {
+		await postinstall.process(obj[process.env.npm_lifecycle_event] || {}, opts);
+		process.exit(0);
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
+})();
